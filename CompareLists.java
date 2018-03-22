c1aa5457e1cd0436c650db6468dc3a32be3ff138
Gourav Rusiya <gouravrusiya.lnct@gmail.com>
2018-03-22 19:37:39 +0530

    Compare two linked lists
    You’re given the pointer to the head nodes of two linked lists. Compare the data in the nodes of the linked lists to check if they are equal. The lists are equal only if they have the same number of nodes and corresponding nodes contain the same data. Either head pointer given may be null meaning that the corresponding list is empty.

---
+++ CompareLists.java
@@ -0,0 +1,22 @@
+/*
+  Compare two linked lists A and B
+  Return 1 if they are identical and 0 if they are not. 
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+int CompareLists(Node headA, Node headB) {
+    // This is a "method-only" submission. 
+    // You only need to complete this method
+    
+    while(headA!=null || headB!=null){   
+        if(headA==null || headB==null || (headA.data != headB.data))
+            return 0;
+        
+        headA = headA.next;
+        headB = headB.next;
+    }
+    return 1;
+}
